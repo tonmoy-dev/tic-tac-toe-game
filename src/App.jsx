22809f8eb61f92ca2bec8cc3760be77d17288095
tonmoy-dev <rxdevs100@gmail.com>
2024-01-14 16:31:02 +0600

    game component added with history feature

--- src/App.jsx
+++ src/App.jsx
@@ -27,14 +27,8 @@ function Square({ value, onSquareClick }) {
 }
 
 // Board component
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null)); // 9 elements in the array
-  // console.log(squares); //  [null, null, null, null, null, null, null, null, null]
-
-  // react re-renders the JSX when, we change the state value by any interactions.
-  const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(squares);
-  // console.log(winner); // X / O
+function Board({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares); // winner => X / O
 
   let status;
   if (winner) {
@@ -45,7 +39,7 @@ export default function Board() {
 
   // click event handler function
   function handleClick(i) {
-    if (squares[i] || winner) {
+    if (squares[i] || calculateWinner(squares)) {
       // Get out from the handler function if there is any value in i (squares[i] == 1).
       // And there is any winner!
       return;
@@ -57,43 +51,98 @@ export default function Board() {
       nextSquares[i] = "X";
     } else nextSquares[i] = "O";
 
-    setSquares(nextSquares); // updating the squares array
-    setXIsNext(!xIsNext); // Toggling the xIsNext state/ to the another player.
+    onPlay(nextSquares); // updating the handler while playing
   }
-  // console.log(squares);
 
   return (
     <>
-      <div className="grid  place-items-center place-content-center">
-        <h1 className="text-2xl mb-3">Tic Tac Toe Game!</h1>
-        <p>{status}</p>
-        {/* We need 9 squares to build the game */}
-        <div className="flex">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
+      <h1 className="text-2xl mb-3">Tic Tac Toe Game!</h1>
+      <p>{status}</p>
+      {/* We need 9 squares to build the game */}
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
 
-        <div className="flex">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
 
-        <div className="flex">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-          {/* Here, the props are passing as a object to the Square component */}
-        </div>
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        {/* Here, the props are passing as a object to the Square component */}
       </div>
     </>
   );
 }
 
-// calculating the winner --- this function will check every time.
+// History component
+function History({ moves }) {
+  return <>{moves}</>;
+}
+
+// Game component => Root level component
+export default function Game() {
+  // creating a array of array elements for creating the history
+  const [history, setHistory] = useState([Array(9).fill(null)]); // 9 elements a the array with 9 arrays
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove]; // latest move
+
+  function handlePlay(nextSquares) {
+    setXIsNext(!xIsNext); // Toggling the xIsNext state/ to the another player.moves
+    // Taking the moves from 0 to latest.
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory); // updating the history arrays
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  // time travelling to the game history
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0); // if move is even (starts from 0) -> player "X", otherwise "0" will play.
+  }
+  // looping through the history array
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      // game starts
+      description = `Go to move # ${move}`;
+    } else {
+      description = `Go to start the game`;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="flex items-center justify-center gap-5 py-10">
+      <div>
+        {/* Game Board */}
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+
+      <div>
+        <ol>
+          {/* Game History */}
+          <History moves={moves} />
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+// calculating the winner => this function will check every time.
 function calculateWinner(squares) {
-  // console.log(squares); // squares array will pass here with every clicks of any player
   const winningLines = [
     // horizontal lines
     [0, 1, 2],
@@ -110,14 +159,12 @@ function calculateWinner(squares) {
   // looping over the winning lines
   for (let i = 0; i < winningLines.length; i++) {
     const [a, b, c] = winningLines[i];
-
-    // console.log(a, b, c); // Ex. 0, 3, 6 -- these are winning lines.
-    // console.log(squares[a], squares[b], squares[c]);
-    // Ex. X, X, X; squres[0] == X; squares[3] == X; squares[6] == X;
-
+    // checking for the winner
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]; // there is a winner!
     }
+    // Ex. (a,b,c) => (0, 3, 6) => winning lines.
+    // Ex. if it is X, X, X; Then, squres[0] == X; squares[3] == X; squares[6] == X;
   }
   return null; // there is no winner
 }
